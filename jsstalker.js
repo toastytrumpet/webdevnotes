e56541bf7914d3bfbde7add99eaab5ca7e9e3c27
Toshiro Chun <toshiro.chun@gmail.com>
2019-04-30 21:54:59 -0400

    notes

---
+++ jsstalker.js
@@ -0,0 +1,6 @@
+var firstName = prompt ("what is your first name?"); 
+var lastName = prompt ("what is your last name?");
+var age = prompt ("How old are you?");
+
+console.log ("Your full name is " + firstName + " " + laneName);
+console.log ("You are" + age + " " "years old");
